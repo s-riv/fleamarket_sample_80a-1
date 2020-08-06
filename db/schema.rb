5649df00d7dd6a33358e39b1614cb7e4aa8f140e
github kinop1987 <kinop1987@gmail.com>
2020-08-06 15:10:00 +0900

    new_devise_user3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_05_105557) do
+ActiveRecord::Schema.define(version: 2020_08_06_060906) do
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -21,6 +21,11 @@ ActiveRecord::Schema.define(version: 2020_08_05_105557) do
     t.string "brand"
     t.string "ship_day", null: false
     t.string "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
