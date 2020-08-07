5908e7e9c74d8edd75a3077cb77ba24ee1a22102
github kinop1987 <kinop1987@gmail.com>
2020-08-07 10:33:30 +0900

    design_association_and_validation
    モデル間にアソシエーションとバリデーションを行いました。
    確認をお願いします。

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,10 @@
 class Address < ApplicationRecord
+  belongs_to :user, optional: true
+
+  with_options presence: true do
+    validates :postal_code
+    validates :prefecture_id
+    validates :municipality
+    validates :apartment_name
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable
 
+  has_one :address, dependent: :delete
+  has_many :products, dependent: :delete_all
+
   with_options presence: true do
     validates :nickname
     validates :birthday
