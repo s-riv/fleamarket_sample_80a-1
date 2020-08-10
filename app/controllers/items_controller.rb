879dbae79aa66c86a14ddcc2b28415c1d8000d53
kofuken1815 <mentaikofuna@gmail.com>
2020-08-10 20:00:40 +0900

    change-markup

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,5 +2,6 @@ class ItemsController < ApplicationController
   def index
     @products = Product.all.limit(5)
     @parents = Category.all.order("id ASC").limit(13)
+    @images = Image.find(1)
   end
 end
