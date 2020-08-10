d6c2307d180b22433adfe0dd91398107f69df4b9
kofuken1815 <mentaikofuna@gmail.com>
2020-08-10 20:08:44 +0900

    add

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   def index
     @products = Product.all.limit(5)
-    @parents = Category.all.order("id ASC").limit(13)
+    @parents = Category.all.order("id ASC").limit(4)
     @images = Image.find(1)
   end
 end
