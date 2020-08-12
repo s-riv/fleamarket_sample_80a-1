7315785ac0f2ca393f3277df0bc53e6550b937be
github kinop1987 <kinop1987@gmail.com>
2020-08-12 20:51:28 +0900

    商品出品機能のエラーの解決
    モデルとアプリケーションコントローラーに必要な記述を行い、エラーを解消しました。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_ancestry
   protect_from_forgery with: :exception
+
 
   private
 
@@ -18,6 +20,9 @@ class ApplicationController < ActionController::Base
                  :last_name, :first_name_kana, :last_name_kana,])
   end
 
+  def set_ancestry
+    @parent_categories = Category.where(ancestry: nil)
+  end
 
 
   def production?
