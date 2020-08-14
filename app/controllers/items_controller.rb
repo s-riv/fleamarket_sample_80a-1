ebf6b323f098816153493958dd548bd8bda9d68d
kuriken0410 <67869934+kuriken0410@users.noreply.github.com>
2020-08-14 19:18:24 +0900

    カテゴリ機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only:[:show, :destroy, :edit, :update, :purchase, :payment]
+  
   def index
     @products = Product.all.includes(:images).limit(5)
   end
+
 end
