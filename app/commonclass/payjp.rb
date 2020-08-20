a5d0dccc9f371f59db468a8567dd582ff0cfd29a
github kinop1987 <kinop1987@gmail.com>
2020-08-20 17:23:07 +0900

    edit_pay.jp

--- app/commonclass/payjp.rb
+++ app/commonclass/payjp.rb
@@ -1,8 +1,7 @@
 require 'payjp'
 
 class MyPayjp
-  Payjp::api_key = ENV['PAYJP_PRIVATE_KEY']
-
+  Payjp.api_key = Rails.application.credentials.payjp[:api_secret_key]
   def self.create_token(number, cvc, exp_month, exp_year)
     token = Payjp::Token.create(
       card: {
