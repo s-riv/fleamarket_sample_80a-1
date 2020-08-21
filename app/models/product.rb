e83295aa7e1457887dbabea8d959735e5340d012
kofuken1815 <mentaikofuna@gmail.com>
2020-08-21 11:04:57 +0900

    display-condition-sendprice-ship_day

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,10 +7,10 @@ class Product < ApplicationRecord
   has_one :contract
   accepts_nested_attributes_for :images, allow_destroy: true
 
-  enum condition:   { brand_new: 0, near_to_unused: 1, no_scrach: 2, slight_scrach: 3, has_scrach: 4, bad_state: 5 }
+  enum condition:   { 新品／未使用: 0, 未使用に近い: 1, 目立った傷や汚れなし: 2, やや傷や汚れあり: 3, 傷や汚れあり: 4, 全体的に状態が悪い: 5 }
   enum status:      { display: 0, sold: 1 }
-  enum send_price:  { listing_person: 0, buyer_burden: 1 }
-  enum ship_day:    { early_ship: 0, mid_ship: 1, late_ship: 2 }
+  enum send_price:  { 出品者負担: 0, 購入者負担: 1 }
+  enum ship_day:    { １〜２日で発送: 0, ２〜３日で発送: 1, ４〜７日で発送: 2 }
 
   with_options presence: true do
     validates :name
